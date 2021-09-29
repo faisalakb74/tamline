5b55e257ad3532743fe6385e0437c3930a35b79c
faisalakbar <faisall.akbar@gmail.com>
2021-09-29 19:42:24 +0700

    first commit

---
+++ app/src/main/java/com/example/tamline/loginPenggunaActivity.java
@@ -0,0 +1,37 @@
+package com.example.tamline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class loginPenggunaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_pengguna);
+
+        final TextView lupaPassword = (TextView) findViewById(R.id.lupaPasswordPengguna);
+        lupaPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(loginPenggunaActivity.this, lupaPasswordActivity.class);
+                startActivity(myIntent);
+                return;
+            }
+        });
+
+        final TextView daftarPengguna = (TextView) findViewById(R.id.daftarPengguna);
+        daftarPengguna.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(loginPenggunaActivity.this, signupPenggunaActivity.class);
+                startActivity(myIntent);
+                return;
+            }
+        });
+    }
+}
