f468094536d131257fe84350454f31bdb4367346
faisalakbar <faisall.akbar@gmail.com>
2021-10-01 15:49:45 +0700

    updated

--- app/src/main/java/com/example/tamline/loginBengkelActivity.java
+++ app/src/main/java/com/example/tamline/loginBengkelActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class loginBengkelActivity extends AppCompatActivity {
@@ -31,6 +32,13 @@ public class loginBengkelActivity extends AppCompatActivity {
                 Intent myIntent = new Intent(loginBengkelActivity.this, signupBengkelActivity.class);
                 startActivity(myIntent);
                 return;
+            }
+        });
+        Button masuk = findViewById(R.id.buttonMasukBengkel);
+        masuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(loginBengkelActivity.this, akunActivity.class));
             }
         });
     }
--- app/src/main/java/com/example/tamline/loginPenggunaActivity.java
+++ app/src/main/java/com/example/tamline/loginPenggunaActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class loginPenggunaActivity extends AppCompatActivity {
@@ -31,6 +32,14 @@ public class loginPenggunaActivity extends AppCompatActivity {
                 Intent myIntent = new Intent(loginPenggunaActivity.this, signupPenggunaActivity.class);
                 startActivity(myIntent);
                 return;
+            }
+        });
+
+        Button masuk = findViewById(R.id.buttonMasukPengguna);
+        masuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(loginPenggunaActivity.this, akunActivity.class));
             }
         });
     }
